d3e63a7878d6b936eeb95deb85036fa33687de9c
Pedro Rafante <pedrorafante@gmail.com>
2023-02-01 14:35:47 -0300

    Criação do projeto

---
+++ src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -0,0 +1,5 @@
+package med.voll.api.endereco;
+
+public record DadosEndereco(String logradouro, String bairro, String cep, String cidade, String uf, String complemento,
+                            String numedo) {
+}
---
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -0,0 +1,7 @@
+package med.voll.api.medico;
+
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade,
+                                  DadosEndereco endereco) {
+}
