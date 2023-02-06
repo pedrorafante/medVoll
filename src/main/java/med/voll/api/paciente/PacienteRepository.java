3a9a73b563b544320161aa774a50c4e98a2d1ff1
Pedro Rafante <pedrorafante@gmail.com>
2023-02-06 09:29:04 -0300

    Implementado a listagem de pacientes

--- src/main/java/med/voll/api/paciente/PacienteRepository.java
+++ src/main/java/med/voll/api/paciente/PacienteRepository.java
@@ -1,6 +1,10 @@
 package med.voll.api.paciente;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+
+    Page<Paciente> findAllByAtivoTrue(Pageable pageable);
 }
